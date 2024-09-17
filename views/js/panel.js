15f62f890bc584fbab70f0c659958d638f75e33e
luca <luca@gmail.com>
2024-09-17 14:48:26 -0300

    Cree el archivo del panel.js + Cree el archivo javascript panel.js

--- views/js/panel.js
+++ views/js/panel.js
@@ -1,1 +1,12 @@
-panel.js
+// let ofertas = document.querySelector(".ofertas");
+
+fetch('https://mattprofe.com.ar/proyectos/app-estacion/datos.php?mode=list-stations')
+.then(response => {
+     return response.json()
+    })
+.then(cate => {
+
+console.log(cate);
+
+
+})
